eb63500b574f07ba1d6b96bc8a738fba63cbfdd0
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-18 00:49:14 +0530

    chore: get description of mudule and render

--- src/app/flow-components/drawer.tsx
+++ src/app/flow-components/drawer.tsx
@@ -8,22 +8,42 @@ import {
 } from "@/components/ui/sheet";
 import { useShallow } from "zustand/react/shallow";
 import { useUIStore } from "../stores/useUI";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export const Drawer = () => {
-  const { drawerOpen, toggleDrawer } = useUIStore(
+  const { drawerOpen, toggleDrawer, drawerDetails } = useUIStore(
     useShallow((state) => ({
       drawerOpen: state.drawerOpen,
       toggleDrawer: state.toggleDrawer,
+      drawerDetails: state.drawerDetails,
     }))
   );
+
+  const { data, isSuccess } = useQuery({
+    queryKey: [
+      drawerDetails?.query,
+      drawerDetails?.parent,
+      drawerDetails?.child,
+    ],
+    queryFn: async () => {
+      return await axios.post("/api/v1/openai/details", drawerDetails);
+    },
+    enabled: Boolean(
+      drawerDetails &&
+        drawerDetails?.query &&
+        drawerDetails?.parent &&
+        drawerDetails?.child
+    ),
+  });
+
   return (
     <Sheet open={drawerOpen} onOpenChange={toggleDrawer}>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Are you absolutely sure?</SheetTitle>
           <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            {isSuccess && <>{data.data.text.description}</>}
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
--- src/app/stores/useUI.ts
+++ src/app/stores/useUI.ts
@@ -1,12 +1,26 @@
 import { create } from 'zustand'
+
+type drawerDetails = {
+    query: string,
+    parent: string,
+    child: string,
+}
+
 interface UIState {
     drawerOpen: boolean;
-    // setDrawerOpen: (open: boolean) => void;
-    toggleDrawer: () => void; // added toggle action
+    toggleDrawer: () => void;
+    drawerDetails: drawerDetails | null,
+    setDrawerDetails: ({
+        query,
+        parent,
+        child
+    }: drawerDetails) => void
 }
 
 
 export const useUIStore = create<UIState>((set) => ({
     drawerOpen: false,
     toggleDrawer: () => set((state) => ({ drawerOpen: !state.drawerOpen })),
+    drawerDetails: null,
+    setDrawerDetails: (data) => set(() => ({ drawerDetails: data })),
 }))
