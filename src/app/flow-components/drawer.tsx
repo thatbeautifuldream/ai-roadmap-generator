f6570c895250ed4f5d6b4d08971fa0c7ca6493d4
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-18 02:39:07 +0530

    added books

--- src/app/flow-components/drawer.tsx
+++ src/app/flow-components/drawer.tsx
@@ -1,11 +1,4 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetDescription } from "@/components/ui/sheet";
 import { useShallow } from "zustand/react/shallow";
 import { useUIStore } from "../stores/useUI";
 import { useQuery } from "@tanstack/react-query";
@@ -16,6 +9,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { IOrilley } from "@/lib/types";
+import { formatDuration } from "@/lib/utils";
 
 export const Drawer = () => {
   const { drawerOpen, toggleDrawer, drawerDetails } = useUIStore(
@@ -45,46 +40,93 @@ export const Drawer = () => {
         drawerDetails?.parent &&
         drawerDetails?.child
     ),
+    staleTime: Infinity,
+  });
+
+  const { data: booksData, isSuccess: booksSuccess } = useQuery({
+    queryKey: [
+      "Books",
+      drawerDetails?.query,
+      drawerDetails?.parent,
+      drawerDetails?.child,
+    ],
+    queryFn: async () => {
+      return await axios.post(`/api/v1/orilley`, {
+        data: { query: drawerDetails?.child },
+      });
+    },
+    staleTime: Infinity,
+    retry: false,
+    enabled: Boolean(drawerDetails?.child),
   });
 
   return (
     <Sheet open={drawerOpen} onOpenChange={toggleDrawer}>
       <SheetContent>
-        <SheetHeader>
-          <SheetTitle>{drawerDetails?.parent}</SheetTitle>
-          <SheetDescription>
-            {isSuccess && (
-              <div>
-                <div className="flex flex-wrap mb-2">
-                  {data.data.text.link && (
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger>
-                          {" "}
-                          <a
-                            href={data.data.text.link}
-                            target="_blank"
-                            referrerPolicy="no-referrer"
-                          >
-                            <img
-                              src={`https://en.wikipedia.org/static/images/icons/wikipedia.png`}
-                              alt="Wikipedia Logo"
-                              className="w-4"
-                            />
-                          </a>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Wikipedia</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
+        <div className="mb-4">
+          <p className="text-xs text-slate-400">{drawerDetails?.parent}</p>
+          <p className="font-light">{drawerDetails?.child}</p>
+        </div>
+        <div>
+          {isSuccess && (
+            <div>
+              <div className="flex flex-wrap mb-2">
+                {data.data.text.link && (
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <a
+                          href={data.data.text.link}
+                          target="_blank"
+                          referrerPolicy="no-referrer"
+                        >
+                          <img
+                            src={`https://en.wikipedia.org/static/images/icons/wikipedia.png`}
+                            alt="Wikipedia Logo"
+                            className="w-4"
+                          />
+                        </a>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Wikipedia</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                )}
+              </div>
+              <p className="text-sm text-slate-600">
+                {data.data.text.description}
+              </p>
+              <div className="mt-4">
+                <p className="text-black mb-2">Recommended Books</p>
+                <div className="flex flex-col gap-3">
+                  {booksData?.data?.data?.results?.map(
+                    (book: IOrilley["data"][number], id: number) => (
+                      <a
+                        className="shadow-md px-2 py-2"
+                        href={book.url}
+                        target="_blank"
+                        key={book.id}
+                      >
+                        <p className="mb-2">{book.title}</p>
+                        <div className="flex gap-2">
+                          <img className="w-20" src={book.cover_url} />
+                          <div>
+                            <p>By {book.authors[0]}</p>
+                            <p>
+                              Complete in{" "}
+                              {formatDuration(book.duration_seconds)}
+                            </p>
+                          </div>
+                        </div>
+                      </a>
+                    )
                   )}
                 </div>
-                <p>{data.data.text.description}</p>
               </div>
-            )}
-          </SheetDescription>
-        </SheetHeader>
+            </div>
+          )}
+        </div>
       </SheetContent>
     </Sheet>
   );
---
+++ src/lib/types.ts
@@ -0,0 +1,44 @@
+export interface IOrilley {
+    data: {
+        id: 'https://www.safaribooksonline.com/api/v1/book/9781805127826/',
+        archive_id: '9781805127826',
+        ourn: 'urn:orm:video:9781805127826',
+        last_modified_time: '2023-12-01T18:15:21.145Z',
+        issued: '2023-04-20T00:00:00Z',
+        format: 'video',
+        content_format: 'video',
+        authors: ['Brad Traversy'],
+        publishers: ['Packt Publishing'],
+        academic_excluded: false,
+        language: 'en',
+        title: 'Modern JavaScript from the Beginning - Second Edition',
+        description:
+        '<span><p>This course is a comprehensive introduction to JavaScript, covering everything from the basics of variables, data types, and methods, to more advanced topics such as asynchronous programming, object-oriented programming, and unit testing.</p><p>Starting with the fundamentals of the language, the course progresses through a series of carefully structured sections that build on each other to give you a solid understanding of JavaScript’s capabilities and how to use them effectively. You will learn how to work with arrays and objects, write functions, and use logic and control flow to create more complex programs.</p><p>From there, the course dives into more advanced topics such as DOM manipulation, event handling, and asynchronous programming, teaching you how to use these techniques to create responsive and interactive web applications. You will also learn about object-oriented programming in JavaScript, including constructors, prototypes, and classes, and build several projects along the way to put your new skills into practice.</p><p>The course also covers more advanced topics such as modules, iterators, and generators, as well as unit testing and data structures. Additionally, you will get hands-on experience with tools like Webpack and Node.js, giving you a well-rounded understanding of modern JavaScript development.</p><p>By the end of the course, you will have a solid foundation in JavaScript and be able to build dynamic, interactive web applications using the language. Whether you are just starting out with programming or are looking to expand your skillset, this course is the perfect way to learn JavaScript from the ground up.</p><h4>What You Will Learn</h4><ul><li>Create and manipulate variables, data types, methods, and objects</li><li>Understand control flow, loops, and iteration in JavaScript</li><li>Build web applications using DOM manipulation and event handling</li><li>Use asynchronous JavaScript to make web applications more efficient</li><li>Implement object-oriented programming principles in JavaScript</li><li>Build real-world projects such as a shopping list app and movie app</li></ul><h4>Audience</h4><p>This course is designed for beginners who are new to programming and have little to no experience with JavaScript. It is also suitable for anyone who wants to refresh their knowledge of JavaScript and its fundamentals. This course is ideal for web developers who want to enhance their skills and build dynamic and interactive web applications. Additionally, anyone interested in pursuing a career in front-end development or web design would find this course helpful. Finally, those who want to gain experience with modern JavaScript tools and frameworks, such as Node.js and React, would benefit from this course as it provides a strong foundation in JavaScript programming.</p><h4>About The Author</h4><p><b>Brad Traversy:</b> Brad Traversy has been programming for around 12 years and teaching for almost 5 years. He is the owner of Traversy Media, which is a successful web development YouTube channel and specializes in everything from HTML5 to front-end frameworks such as Angular as well as server-side technologies such as Node.js, PHP, and Python. Brad has mastered explaining overly complex topics in a simple manner that is understandable. Invest in your knowledge by watching Brad’s courses.</p><br><strong id="content-levels">Content Level: Beginner, Intermediate</strong><br><strong id="has_badge">With Badge</strong></span>',
+        url: 'https://learning.oreilly.com/api/v1/book/9781805127826/',
+        web_url: '/videos/modern-javascript-from/9781805127826/',
+        source: 'application/epub+zip',
+        content_type: 'book',
+        duration_seconds: 133218,
+        video_classification: 'structured-learning',
+        has_assessment: false,
+        timestamp: '2023-12-01T18:15:42.779Z',
+        average_rating: 4636,
+        number_of_followers: 0,
+        number_of_items: 0,
+        number_of_reviews: 11,
+        popularity: 999101044,
+        report_score: 51000,
+        cover_url: 'https://learning.oreilly.com/library/cover/9781805127826/',
+        date_added: '2023-05-03T05:19:09.382Z',
+        topics: ['a0765028-6564-479c-ad97-0bbb7cd80a8f'],
+        topics_payload: [
+            {
+                uuid: 'a0765028-6564-479c-ad97-0bbb7cd80a8f',
+                slug: 'javascript',
+                name: 'JavaScript',
+                score: null
+            }
+        ],
+        minutes_required: 2220.3
+    }[]
+}
