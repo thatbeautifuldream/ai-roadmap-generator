156f01590fee703ef35f57b0d4ac8eb91c5110de
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-18 20:32:29 +0530

    add ReactQueryDevtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,17 @@
 
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient();
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
