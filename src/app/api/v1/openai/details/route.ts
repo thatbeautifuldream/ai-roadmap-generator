98ddf2862cd1d800e6c08c5332d9a5e304708d25
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-18 23:48:53 +0530

    [bug-fix]: fallback api key

--- src/app/api/v1/openai/details/route.ts
+++ src/app/api/v1/openai/details/route.ts
@@ -2,7 +2,7 @@ import { OpenAI } from "openai";
 import { NextResponse } from "next/server";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || 'MY_API_KEY',
 });
 
 export const POST = async (req: Request, res: Response) => {
