ebc2dd7456d4ca15f448651ba638ec9322a171b6
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-20 17:54:01 +0530

    added beta tag

--- src/app/roadmap/index.tsx
+++ src/app/roadmap/index.tsx
@@ -97,12 +97,18 @@ export default function Roadmap() {
     <>
       <div className="h-full flex-col md:flex">
         <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-          <Link
-            className="hidden sm:inline text-lg font-semibold mr-4"
-            href="/"
-          >
-            RoadmapAI
-          </Link>
+          <div className="relative">
+            <Link
+              className="hidden sm:inline text-lg font-semibold mr-4"
+              href="/"
+            >
+              RoadmapAI
+            </Link>
+            <span className="hidden sm:inline top-[22px] absolute left-[72px] items-center rounded-md bg-gray-100 px-1.5 py-0.5 text-[10px] font-medium text-gray-700">
+              BETA
+            </span>
+          </div>
+
           <div className="ml-auto flex w-full space-x-2 sm:justify-end">
             <Input
               type="text"
