d38412043389696e56379bdc6dcccbafd6fbdfd2
Milind Mishra <milind.mishra4@gmail.com>
2024-03-21 18:12:36 +0530

    [NEW] add avatar and created at to dashboard cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getRoadmapsByUserId } from "@/actions/roadmaps";
-
+import moment from "moment";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default async function Dashboard() {
@@ -17,7 +17,14 @@ export default async function Dashboard() {
               <CardContent>
                 <div className="flex items-center justify-between">
                   <span className="text-sm">1 views</span>
-                  <span className="text-sm text-gray-400">2m ago</span>
+                  <span className="text-sm text-gray-400">
+                    {moment(roadmap.createdAt).startOf("hour").fromNow()}
+                  </span>
+                  <img
+                    className="inline-block h-6 w-6 rounded-full"
+                    src="https://lh3.googleusercontent.com/a/ACg8ocLNkTBbLQLLrZMz3vdKW8x37P1usXna6T5SYvhYCgS_iiSP=s96-c"
+                    alt=""
+                  />
                 </div>
               </CardContent>
             </Card>
