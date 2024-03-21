981780bf2465b929541724595409f19b603a4ee9
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-21 17:22:11 +0530

    dashboard page improvement

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,15 @@
-import { db } from "@/lib/db";
-import { auth } from "@/auth";
+import { getRoadmapsByUserId } from "@/actions/roadmaps";
 
 const dashboard = async () => {
-  const session = await auth();
-  const userId = session?.user?.id;
-  console.log(userId);
-  const roadmaps = await db.roadmap.findMany({
-    where: {
-      userId,
-    },
-  });
-  console.log(roadmaps);
-  return <h1>{JSON.stringify(roadmaps)}</h1>;
+  const roadmaps = await getRoadmapsByUserId();
+
+  return (
+    <div>
+      {roadmaps?.map((roadmap) => (
+        <div key={roadmap.id}>{roadmap.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default dashboard;
