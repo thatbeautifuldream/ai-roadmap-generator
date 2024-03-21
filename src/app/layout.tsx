e2ec058b8a4fb599230eb8b582261329e487a12f
Milind Mishra <milind.mishra4@gmail.com>
2024-03-22 04:27:57 +0530

    [NEW] FONT-CHANGE nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import AppBar from "@/app/appbar";
 import Providers from "@/app/providers";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AI Roadmap Generator",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={nunito.className}>
         <Providers>
           <AppBar />
           {children}
