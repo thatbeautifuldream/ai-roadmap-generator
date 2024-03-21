f11d03f31dbd867fe3241eb996e579ad19572b28
SuyashPatil-29 <patilsuyash892@gmail.com>
2024-03-22 02:18:41 +0530

    AppBar

--- src/app/appbar.tsx
+++ src/app/appbar.tsx
@@ -5,23 +5,28 @@ async function AppBar() {
   const session = await auth();
   return (
     <div className="p-2 flex gap-2 items-center">
-      <Link className="text-black font-semibold ml-2" href="/">
-        RoadmapAI
-      </Link>
       {/* Move the two links to the left */}
       {session && session.user && (
-        <>
-          <Link className="font-semibold text-sm" href={"/roadmap"}>
-            Roadmap
-          </Link>
+        <div className="flex gap-4 ml-6">
+          <div className="relative">
+            <Link
+              className="hidden sm:inline text-lg font-semibold mr-4"
+              href="/"
+            >
+              RoadmapAI
+            </Link>
+            <span className="hidden sm:inline top-[22px] absolute left-[72px] items-center rounded-md bg-gray-100 px-1.5 py-0.5 text-[10px] font-medium text-gray-700">
+              BETA
+            </span>
+          </div>
           <Link className="font-semibold text-sm" href={"/dashboard"}>
             Dashboard
           </Link>
-        </>
+        </div>
       )}
       <div className="ml-auto">
         {session && session.user ? (
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-8 items-center mr-12">
             <p className="font-semibold text-sm">{session.user.name}</p>
             <form
               action={async () => {
