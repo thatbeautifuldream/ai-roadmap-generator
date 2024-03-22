97dbdbd136de00ef5e21edbd594d65fbbc270c37
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-22 21:06:21 +0530

    ts-fixes

--- src/app/api/v1/cohere/details/route.ts
+++ src/app/api/v1/cohere/details/route.ts
@@ -1,3 +1,4 @@
+import { JSONType } from "@/lib/types";
 import { SanitiseJSON } from "@/lib/utils";
 import { ChatCohere } from "@langchain/cohere";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
@@ -32,7 +33,7 @@ export const POST = async (req: Request, res: Response) => {
 
     // const chain = prompt.pipe(model);
 
-    // let json: any = {};
+    // let json = {};
 
     // const response = await chain.invoke({
     //   input: `Generate a roadmap in JSON format related to the title: ${query} which has the JSON structure: {query: ${query}, chapters: {chapterName: string[]}}.`,
@@ -50,11 +51,20 @@ export const POST = async (req: Request, res: Response) => {
       input: `a roadmap in JSON format has been generated related to the title: ${query} which has the JSON structure: {query: ${query}, chapters: {chapterName: [{moduleName: string, moduleDescription: string, link?: string}]}} and i'd like to request a small description and wikipedia link on moduleName: ${child} from chapterName: ${parent} in JSON format: {description: string, link: string}`,
     });
 
-    let json: any = {};
+    let json: JSONType | null = null;
 
     try {
       const data = SanitiseJSON(String(response?.content));
       json = JSON.parse(data);
+      if (!json) {
+        return NextResponse.json(
+          {
+            status: false,
+            message: "Error parsing roadmap data.",
+          },
+          { status: 500 }
+        );
+      }
 
       return NextResponse.json({ status: true, text: json }, { status: 200 });
     } catch (e) {
