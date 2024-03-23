85ed06cc1eaed1457d04d18aaccbb262ad8902dc
syndg <sinchandsgpt9@gmail.com>
2024-03-23 12:01:30 +0530

    🐛 FIX: Search on explore page

--- src/components/flow-components/Search.tsx
+++ src/components/flow-components/Search.tsx
@@ -6,6 +6,7 @@ import { timeFromNow } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import { EmptyAlert } from "../alerts/EmptyAlert";
 import RoadmapCard from "./roadmap-card";
+import { Loader2 } from "lucide-react";
 
 export const Search = () => {
   const [search, setSearch] = useState("");
@@ -22,30 +23,24 @@ export const Search = () => {
 
   useEffect(() => {
     onSearch();
-  }, []);
+  }, [search]);
 
   return (
     <>
       <div className="flex flex-row">
         <Input value={search} onChange={(e) => setSearch(e.target.value)} />
-        <Button isLoading={isSearchPending} onClick={onSearch}>
-          Search
-        </Button>
+        <Button onClick={onSearch}>Search</Button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 mb-10">
-        {searchData?.data?.length > 0 ? (
-          searchData.data.map((roadmap: any) => (
-            <RoadmapCard
-              key={roadmap.id}
-              title={roadmap.title}
-              views="2 views"
-              timeAgo={timeFromNow(roadmap?.createdAt?.toString())}
-              slug={roadmap.id}
-            />
-          ))
-        ) : (
-          <EmptyAlert description="You haven't created any roadmaps yet. Please create one to get started." />
-        )}
+        {searchData?.data.map((roadmap: any) => (
+          <RoadmapCard
+            key={roadmap.id}
+            title={roadmap.title}
+            views="2 views"
+            timeAgo={timeFromNow(roadmap?.createdAt?.toString())}
+            slug={roadmap.id}
+          />
+        ))}
       </div>
     </>
   );
