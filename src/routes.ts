e7755743ffdc955efaab70f8ac175a77f421b759
Milind Mishra <milind.mishra4@gmail.com>
2024-03-28 05:29:55 +0530

    [NEW] new public routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", '/api/health', '/explore'];
+export const publicRoutes = ["/", "/api/health", "/explore", "/roadmap/:id"];
 
 /**
  * An array of routes that are used for authentication
