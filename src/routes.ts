f3fa4d8e478b1545b8a8f59207a301fe069a036a
Milind Mishra <milind.mishra4@gmail.com>
2024-03-28 11:06:19 +0530

    [NEW] pricing and hero text update

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/", "/api/health", "/explore", "/roadmap/:id"];
+export const publicRoutes = ["/", "/api/health", "/explore", "/roadmap(.*)"];
 
 /**
  * An array of routes that are used for authentication
