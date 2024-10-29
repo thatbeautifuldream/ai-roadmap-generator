1864ff4b50cda76c88bdbde7eb50bb045f9dd2b1
Milind Mishra <milind.mishra4@gmail.com>
2024-10-30 03:00:34 +0530

    feat: update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/health",
+  "/api/webhook(.*)",
+  "/api/og/(.*)",
+  "/explore",
+  "/roadmap/(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
-  publicRoutes: [
-    "/",
-    "/api/health",
-    "/api/webhook(.*)",
-    "/api/og/(.*)",
-    "/explore",
-    "/roadmap/(.*)",
-  ];
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
   matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
